a696438b23999ffecfefbe577f1e8da1f1056f11
Marian <dudychmarian@gmail.com>
2021-08-05 22:06:13 +0100

    Removal of commented out code

--- src/api/index.js
+++ src/api/index.js
@@ -8,5 +8,22 @@ import { request } from './helpers';
  */
 // TODO: All API related logic should be made inside this function.
 export default async function getData() {
-  return [];
+  const vehicles = await fetch('/api/vehicles.json').then((data) => data.json());
+  try {
+    const vehicleResponses = await Promise.all(
+      vehicles.map((v) => fetch(v.apiUrl)
+        .then((r) => r.json())
+        .catch((error) => ({ error, v })))
+    );
+
+    return vehicles.map((v, i) => ({
+      ...v,
+      price: vehicleResponses[i].price ? vehicleResponses[i].price : null,
+      description: vehicleResponses[i].description ? vehicleResponses[i].description : null,
+    }));
+  } catch (errors) {
+    console.log(errors);
+  }
+
+  return vehicles;
 }
